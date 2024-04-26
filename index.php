9d03b682cfaefdcb1c6ea95eb0faac4846ea8801
Hamdija Salkic <salkic.hamdija@gmail.com>
2024-04-26 21:09:45 +0200

    ponuda part

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 
   <?php include 'template_parts/main-header.php'; ?> 
 
+  <?php include 'template_parts/part-ponuda.php'; ?> 
+
   <?php include 'template_parts/footer.php'; ?> 
 
 
---
+++ template_parts/part-ponuda.php
@@ -0,0 +1,34 @@
+<!-- ponuda  -->
+<section class="ponuda container">
+<h2>ŠTA VAM NUDIMO</h2>
+    <p class="prvi">Nullam dictum vehicula lobortis. Nulla leo leo, pulvinar non ex vitae, pulvinar congue nibh. Suspendisse ac ex tincidunt, interdum tortor nec, varius quam. Quisque sit amet magna massa.</p>
+    <hgroup>
+    <article>
+        <div>
+            <img src="img/coffee1.jpg" alt="">
+           <div>
+           <h3>DORUČAK DO 11</h3>
+            <p>Curabitur vestibulum ut enim in suscipit. Nam faucibus erat rhoncus augue ultrices, in auctor orci dapibus. Proin pellentesque mauris velit, at gravida mi fringilla quis. Aenean porta dui ac est ornare, nec tincidunt risus porta.</p>
+           </div>
+        </div>
+    </article>
+    <article>
+        <div>
+            <img src="img/coffee3.jpg" alt="">
+            <div>
+            <h3>UKUSNI SLATKIŠI</h3>
+            <p>Curabitur vestibulum ut enim in suscipit. Nam faucibus erat rhoncus augue ultrices, in auctor orci dapibus. Proin pellentesque mauris velit, at gravida mi fringilla quis. Aenean porta dui ac est ornare, nec tincidunt risus porta.</p>
+            </div>
+        </div>
+    </article>
+    <article>
+        <div>
+            <img src="img/coffee2.jpg" alt="">
+            <div>
+            <h3>PROSLAVE</h3>
+            <p>Curabitur vestibulum ut enim in suscipit. Nam faucibus erat rhoncus augue ultrices, in auctor orci dapibus. Proin pellentesque mauris velit, at gravida mi fringilla quis. Aenean porta dui ac est ornare, nec tincidunt risus porta.</p>
+            </div>
+        </div>
+    </article>
+    </hgroup>
+</section>

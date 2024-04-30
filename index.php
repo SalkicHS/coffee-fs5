dc33117d1805dc9598b4a12ff28830cafddea660
Hamdija Salkic <salkic.hamdija@gmail.com>
2024-04-30 22:00:33 +0200

    dodavanje part usluge

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
+<?php include 'template_parts/header.php'; ?> 
 
+<?php include 'template_parts/main-header.php'; ?> 
 
-  
-  <?php include 'template_parts/header.php'; ?> 
+<?php include 'template_parts/part-ponuda.php'; ?> 
 
-  <?php include 'template_parts/main-header.php'; ?> 
+<?php include 'template_parts/part-meni.php'; ?> 
 
-  <?php include 'template_parts/part-ponuda.php'; ?> 
+<?php include 'template_parts/part-usluge.php'; ?> 
 
-  <?php include 'template_parts/footer.php'; ?> 
+<?php include 'template_parts/footer.php'; ?> 
 
 
 
---
+++ template_parts/part-meni.php
@@ -0,0 +1,71 @@
+
+<div class="meni">
+    <div>
+       <h2>MENI</h2>
+        <div class="row">
+          <div class="meni-item">
+           <div class="img">
+             <img src="img/coffee3.jpg" alt="">
+            </div>
+            <div class="text">
+            <h3>KAFA I KROFNA</h3>
+            <P>Lorem ipsum dolor sit amet.</P>
+            <span>200 din</span>
+            </div>
+          </div>
+          <div class="meni-item">
+           <div class="img">
+             <img src="img/Coffee 7.jpg" alt="">
+            </div>
+            <div class="text">
+            <h3>KAFA I KROASAN</h3>
+            <P>Lorem ipsum dolor sit amet.</P>
+            <span>300 din</span>
+            </div>
+          </div>
+          <div class="meni-item">
+           <div class="img">
+             <img src="img/Coffee 7.jpg" alt="">
+            </div>
+            <div class="text">
+            <h3>PROJA</h3>
+            <P>Lorem ipsum dolor sit amet.</P>
+            <span>290 din</span>
+            </div>
+          </div>
+          <div class="meni-item">
+           <div class="img">
+             <img src="img/coffee 9.jpg" alt="">
+            </div>
+            <div class="text">
+            <h3>JAJA I SUNKARICA</h3>
+            <P>Lorem ipsum dolor sit amet.</P>
+            <span>500 din</span>
+            </div>
+          </div>
+          <div class="meni-item">
+           <div class="img">
+             <img src="img/coffee 10.jpg" alt="">
+            </div>
+            <div class="text">
+            <h3>SMOOTHIE</h3>
+            <P>Lorem ipsum dolor sit amet.</P>
+            <span>900 din</span>
+            </div>
+          </div>
+          <div class="meni-item">
+           <div class="img">
+             <img src="img/coffee 11.jpg" alt="">
+            </div>
+            <div class="text">
+            <h3>SENDVIC</h3>
+            <P>Lorem ipsum dolor sit amet.</P>
+            <span>790 din</span>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div>
+      <img src="img/coffee4.jpg" alt="">
+    </div>
+</div>
